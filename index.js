844873276f9817bd9adb052f155cd95d0062bdf6
MUHAMMADAZIZ <muhammadazizgulomov9@gmail.com>
2025-02-25 09:56:43 +0500

    added

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 /**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
+ * @param {string} s
+ * @return {number}
  */
-var map = function(arr, fn) {
+var romanToInt = function(s) {
     
 };
