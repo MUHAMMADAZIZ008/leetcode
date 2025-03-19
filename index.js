4c54c433854a19e268622a5ad24a63f2e896d671
Muhammadaziz <muhammdazizgulomov9@gmail.com>
2025-03-19 11:04:16 +0500

    done

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * @param {number[]} prices
+ * @return {number}
  */
-var numberGame = function (nums) {
-  const nums2 = nums.sort((a, b) => a - b);
-  const result = [];
-  let bobStep = 1;
-  for (let i = 0; i < nums2.length; i += 2) {
-
-    if (bobStep < nums2.length) {
-      result.push(nums2[bobStep]);
-      result.push(nums2[i]);
-      bobStep += 2;
+var maxProfit = function (prices) {
+  const maxPro = 0;
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      let profit = prices[j] - prices[i];
+      if (profit > maxPro) {
+        maxPro = profit;
+      }
     }
   }
-  return result;
+  return maxPro;
 };
 
-const nums = [5, 4, 2, 3];
-console.log(numberGame(nums));
+// const prices = [7, 1, 5, 3, 6, 4];
+const prices = [7, 6, 4, 3, 1];
+console.log(maxProfit(prices));
