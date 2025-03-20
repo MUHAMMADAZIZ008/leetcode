bbd82b8f50163af945ee7e1615c153f119770115
Muhammadaziz <muhammdazizgulomov9@gmail.com>
2025-03-20 10:52:26 +0500

    first commit

--- index.js
+++ index.js
@@ -1,50 +1,18 @@
 /**
- * @param {number} x
- * @return {number}
+ * @param {number} n
+ * @return {boolean}
  */
-var reverse = function (x) {
-  let reversedNumber = "";
-  let isMinus = false;
-  let num = String(x);
-  if (x < 0) {
-    isMinus = true;
+var isUgly = function (n) {
+  if (n === 1) {
+    return true;
   }
-
-  for (let i = num.length - 1; i >= 0; i--) {
-    if (isMinus) {
-      if (i === num.length - 1) {
-        reversedNumber += "-";
-        reversedNumber += num[i];
-      } else if (num[i] !== "-") {
-        reversedNumber += num[i];
-      }
+  for (let i = 1; i <= 10; i++) {
+    if (n % i === 0 && i % 2 && i !== 2 && i !== 3 && i !== 5) {
+      return false;
     }
-    // if (!isMinus && num[i] !== "0") {
-    //   reversedNumber += num[i];
-    // }
   }
-
-  if (!isMinus) {
-    let num = String(x);
-    let lastPositive = 0;
-    for (let i = 0; i < num.length; i++) {
-      if (num[i] > 0) {
-        lastPositive = i;
-      }
-    }    
-    let num2 = num.split(num[lastPositive]);
-    let result = "";
-    console.log(num2);
-    
-  }
-
-  if (isMinus) {
-    return reversedNumber;
-  }
+  return true;
 };
-// const x = 123
-// const x = -123;
-// const x = 120;
-const x = 1901000;
 
-console.log(reverse(x));
+const  n = 6
+console.log(isUgly(n));
