d6dccb406540639dde1007796f4a89520f12b697
Muhammadaziz <muhammdazizgulomov9@gmail.com>
2025-04-02 15:40:08 +0500

    done

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
 /**
- * @param {string} a
- * @param {string} b
- * @return {string}
+ * @param {number[]} nums
+ * @return {number}
  */
-var addBinary = function (a, b) {
-  let binaryToNumber = 0;
-
-  if (a === "0" && b === "0") {
-    return "0";
-  }
-
-  for (let i = 0; i < a.length; i++) {
-    if (a[a.length - 1 - i] === "1") {
-      binaryToNumber += 2 ** i;
-    }
-  }
-
-  for (let i = 0; i < b.length; i++) {
-    if (b[b.length - 1 - i] === "1") {
-      binaryToNumber += 2 ** i;
-    }
-  }
-
-  let digitalToBinary = "";
-  let result = "";
-  while (binaryToNumber !== 0) {
-    if (binaryToNumber === 1 || binaryToNumber === 0) {
-      digitalToBinary += binaryToNumber;
-      for (let j = digitalToBinary.length - 1; j >= 0; j--) {
-        result += digitalToBinary[j];
+var maximumTripletValue = function (nums) {
+  let maxValue = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if(nums[i] < nums[j] < nums[k]){
+          const value = (nums[i] - nums[j]) * nums[k]
+          if(value > 0 && value > maxValue){
+            maxValue = value
+          }
+        }
       }
-      return result;
     }
-    digitalToBinary += binaryToNumber % 2;
-    binaryToNumber = Math.floor(binaryToNumber / 2);
   }
+  return maxValue
 };
-
-// const a = "11";
-// const b = "1";
-
-(a = "0"), (b = "0");
-// 2+6 = 8, 1+2+6=9
-console.log(addBinary(a, b));
+// const nums = [12,6,1,2,7]
+// const nums = [1,10,3,4,19]
+// const  nums = [1,2,3]
+console.log(maximumTripletValue(nums));
