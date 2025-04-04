0eba882d85cc9dd5df55abebcb5e5e0712e51a59
Muhammadaziz <muhammdazizgulomov9@gmail.com>
2025-04-04 14:58:48 +0500

    done

--- index.js
+++ index.js
@@ -2,23 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maximumTripletValue = function (nums) {
-  let maxValue = 0;
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      for (let k = j + 1; k < nums.length; k++) {
-        if(nums[i] < nums[j] < nums[k]){
-          const value = (nums[i] - nums[j]) * nums[k]
-          if(value > 0 && value > maxValue){
-            maxValue = value
-          }
-        }
-      }
+var maxSum = function (nums) {
+  let newArr = [];
+  nums.forEach((value, index) => {
+    if (newArr.indexOf(value) < 0) {
+      newArr.push(value);
     }
-  }
-  return maxValue
+  });
+  
 };
-// const nums = [12,6,1,2,7]
-// const nums = [1,10,3,4,19]
-// const  nums = [1,2,3]
-console.log(maximumTripletValue(nums));
+
+const  nums = [1,2,3,4,5]
+maxSum(nums)
+
