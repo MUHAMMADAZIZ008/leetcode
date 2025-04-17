a5fa453af060ae03cdb3de41eef418eb0156f594
Muhammadaziz <muhammdazizgulomov9@gmail.com>
2025-04-17 09:36:20 +0500

    done

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 /**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
+ * @param {Function[]} functions
+ * @return {Function}
  */
-var map = function (arr, fn) {
-  return arr.map((item, i) => fn(item, i));
+var compose = function (functions) {
+  return function (x) {
+    let lastValue = x
+    if(!functions.length) {
+      return x
+    }
+    for(let i = functions.length - 1; i >= 0; i--) {
+      lastValue = functions[i](lastValue)        
+    }
+    return lastValue;
+  };
 };
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
