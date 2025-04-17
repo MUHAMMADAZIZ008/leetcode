cd7ac84c767e40e4edaa2fb93919d4c9d668ff12
Muhammadaziz <muhammdazizgulomov9@gmail.com>
2025-04-17 16:48:01 +0500

    done

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 /**
- * @param {Function[]} functions
- * @return {Function}
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
  */
-var compose = function (functions) {
-  return function (x) {
-    let lastValue = x
-    if(!functions.length) {
-      return x
-    }
-    for(let i = functions.length - 1; i >= 0; i--) {
-      lastValue = functions[i](lastValue)        
-    }
-    return lastValue;
-  };
+var argumentsLength = function(...args) {
+  return args.length    
 };
-
-/**
- * const fn = compose([x => x + 1, x => 2 * x])
- * fn(4) // 9
- */
