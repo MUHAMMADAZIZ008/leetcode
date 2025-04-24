0c1326caf62a10ea01a228782b6bb6b82c3a9e99
Muhammadaziz <muhammdazizgulomov9@gmail.com>
2025-04-24 15:19:40 +0500

    done

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 /**
- * @param {Object|Array} obj
- * @return {boolean}
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
  */
-var isEmpty = function (obj) {
-  if (Array.isArray(obj)) {
-    return obj.length ? false : true;
-  } else {
-    return Object.keys(obj).length ? false : true
+var chunk = function (arr, size) {
+  if (!arr.length || !size) return arr;
+  const returnValue = [];
+  let subArr = [];
+  let copyI = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    subArr.push(arr[i]);
+    if (copyI === size) {
+      returnValue.push(subArr);
+      subArr = [];
+      copyI = 0;
+    }
+    if (i === arr.length - 1 && subArr.length) {
+      returnValue.push(subArr);
+    }
+
+    copyI++;
   }
+  return returnValue;
 };
+
+const arr = [1, 2, 3, 4, 5];
+const size = 2;
+console.log(chunk(arr, size));
